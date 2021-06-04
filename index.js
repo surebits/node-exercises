a00ae52aad4a728f5303c74e6faa671a82e4ecc2
surebits <breadphace@gmail.com>
2021-06-04 14:24:06 +0100

    move to root

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const addmod = require("./addmod.js");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (q.pathname == "/add.js") addmod.add(req, res, q.query);
+    else
+      fs.readFile(filename, function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("404 Not Found");
+        }
+        res.writeHead(200); // Content-Type not included
+        res.write(data);
+        return res.end();
+      });
+  })
+  .listen(8080);
